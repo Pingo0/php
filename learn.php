29f0a924723e0a0cc6df1385561d7cc20b85d3b2
Pingo0 <Ping-o0@yndex.ru>
2020-07-13 14:10:43 +0300

    first commit

---
+++ learn.php
@@ -0,0 +1,13 @@
+	<?php 
+    include 'second.php';
+    echo "<br>";
+
+	echo "i am learning php";
+	echo "<br>";
+	?>
+	<h2>Заголовок второго урвня</h2>
+	<h3>Заголовок третьего уровня</h3>
+	<?php 
+	echo date(DATE_RSS);
+?>
+	
